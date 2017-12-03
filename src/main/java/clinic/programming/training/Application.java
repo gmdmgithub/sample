319e72f1a752237fc797cbedca2b3ac4d2f04188
gmdmgithub <gmika@interia.pl>
2017-12-03 21:33:45 +0100

    Use org.apache.commons.lang3.StringUtils and Dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,9 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+	public int countWords(String str){
+		String[] separateWord = StringUtils.split(str,' ');
+		return (separateWord == null)? 0 : separateWord.length;
+	}
+	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
@@ -24,5 +30,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("Hi please tell me how many words?");
+		System.out.println("Sentence with number of words: "+count);
     }
 }
